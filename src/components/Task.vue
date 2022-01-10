4e1b2033aeec7b6d8a1b24809eb596f9b39f17bf
Nisarg Mewada <nisargmewada@gmail.com>
2022-01-10 11:34:24 +0100

    adding files

---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{task.text}}
+            <i class="fas fas-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Task',
+    props:{
+        task: Object,
+    }
+}
+</script>
+
+<style scoped>
+.fas{
+    color: red;
+}
+.task{
+    background: rgb(230, 221, 185);
+    border-color: rgb(180, 91, 91);
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+.task.reminder{
+    border-left: 5px solid rgb(15, 233, 81);
+}
+
+.task h3{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
