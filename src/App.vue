62c9cbb2a9698bb778a86b0a5bc072cb886459f6
Nisarg Mewada <nisargmewada@gmail.com>
2022-01-10 13:02:33 +0100

    adding new features to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"/>
-    <Tasks :tasks="tasks"/>
+    <Header title="My Task Tracker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
 
@@ -20,6 +20,14 @@ export default {
   data(){
     return{
       tasks:[]
+    }
+  },
+  methods: {
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+
+        this.tasks = this.tasks.filter((task)=> task.id !== id)  
+      }
     }
   },
   created() {
